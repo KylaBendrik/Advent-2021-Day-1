6beb27193e42f7fec72e07147874284990b9378f
Beth Jackson <Mbeth.ashton@gmail.com>
2021-12-01 11:15:24 -0500

    Gold Star

--- lib/day1.ex
+++ lib/day1.ex
@@ -1,18 +1,52 @@
 defmodule Day1 do
-  @moduledoc """
-  Documentation for `Day1`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Day1.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  
+  def texts_to_ints(input) do
+    String.split(input, ["\r", "\n", "\r\n"], trim: true)
+    # |> Enum.drop(-1)
+    |> Enum.map(fn x -> String.to_integer(x) end)
+  end
+  
+  def run(input) do
+    input_list = texts_to_ints(input)
+    
+    answer = count_increased(input_list, 0)
+    IO.puts("answer: #{answer}")
+    answer
+  end
+  
+  def run(input, :stage2) do
+    input_list = texts_to_ints(input)
+    # turn list into chunks
+    |> chunkify([])
+    
+    # run count_increased on those chunks
+    answer = count_increased(input_list, 0)
+    IO.puts("answer: #{answer}")
+    answer
+  end
+  
+  def chunkify([a, b, c | [] ], output) do
+    output ++ [a + b + c]
+  end
+  
+  def chunkify([a, b, c | rest ], output) do
+    chunkify([b, c | rest ], output ++ [a + b + c])
+  end
+  
+  def is_increased(a, b) do
+    cond do
+      a < b ->
+        1
+      true ->
+        0
+    end
+  end
+  
+  def count_increased([a, b | []], counted) do
+    counted + is_increased(a, b)
+  end
+  
+  def count_increased([a, b | rest], counted) do
+    count_increased([b | rest], counted + is_increased(a, b))
   end
 end
--- test/day1_test.exs
+++ test/day1_test.exs
@@ -1,6 +1,6 @@
 defmodule Day1Test do
   use ExUnit.Case
-  doctest Day1
+  # doctest Day1
 
   test "example provided" do
     testdata = """
@@ -17,5 +17,56 @@ defmodule Day1Test do
     """
     
     assert Day1.run(testdata) == 7
+  end
+  
+  test "input, stage 1" do
+    {:ok, input} = File.read("lib/input/input.txt")
+    
+    Day1.run(input)
+    
+    assert 1 == 1
+  end
+  
+  test "input, stage 2" do
+    {:ok, input} = File.read("lib/input/input.txt")
+    
+    Day1.run(input, :stage2)
+    
+    assert 1 == 1
+  end
+  
+  test "testdata, stage 2" do
+    testdata = """
+    199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263
+    """
+    
+    assert Day1.run(testdata, :stage2) == 5
+  end
+  
+  test "chunkify testdata" do
+    testdata = """
+    199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263
+    """
+    |> Day1.texts_to_ints()
+    
+    assert Day1.chunkify(testdata, []) == [607, 618, 618, 617, 647, 716, 769, 792]
   end
 end
